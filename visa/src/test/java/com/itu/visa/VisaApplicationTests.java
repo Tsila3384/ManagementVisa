05a9b5d1c7e7c44579da7758cd8bf7413414302d
Tsila3384 <robeltsilavina@gmail.com>
2026-04-23 09:28:44 +0300

    commit   renew

---
+++ visa/src/test/java/com/itu/visa/VisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itu.visa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
